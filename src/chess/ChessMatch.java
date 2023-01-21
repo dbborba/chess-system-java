c025808420aabd18601bd35aac55d1392846dfa2
Diego B <diegoborbas@gmail.com>
2023-01-21 14:10:56 -0300

    Little improvement in board printing

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -30,7 +30,7 @@ public class ChessMatch {
     }
 
     private void initialSetup() {
-        placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+        placeNewPiece('b', 6, new King(board, Color.WHITE));
         placeNewPiece('e', 8, new King(board, Color.BLACK));
         placeNewPiece('e', 1, new King(board, Color.WHITE));
     }
